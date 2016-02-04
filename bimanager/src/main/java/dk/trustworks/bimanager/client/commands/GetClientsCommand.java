4f9936b2d27306445440ff3f706b7e627946a412
hans <hans@godfather.dk>
2016-02-04 21:51:15 +0100

    Fixed import bug

--- bimanager/src/main/java/dk/trustworks/bimanager/client/commands/GetClientsCommand.java
+++ bimanager/src/main/java/dk/trustworks/bimanager/client/commands/GetClientsCommand.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 import dk.trustworks.bimanager.client.RestClient;
 import dk.trustworks.bimanager.dto.Client;
+import dk.trustworks.framework.network.Locator;
 
 import java.util.List;
 
@@ -14,7 +16,6 @@ public class GetClientsCommand {
     }
 
     public List<Client> getClients() {
-        RestClient.log.debug("RestClient.getClients");
         try {
             HttpResponse<JsonNode> jsonResponse;
             jsonResponse = Unirest.get(Locator.getInstance().resolveURL("clientservice") + "/api/clients")
@@ -25,7 +26,6 @@ public class GetClientsCommand {
             });
         } catch (Exception e) {
             e.printStackTrace();
-            RestClient.log.throwing(e);
             throw new RuntimeException("Kunne ikke loade: clients ", e);
         }
     }
