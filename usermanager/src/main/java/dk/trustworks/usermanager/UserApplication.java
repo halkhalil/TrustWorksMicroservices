5e932032478d5ce916c5ec8fc28cbad5a6573a7c
hans <hans@godfather.dk>
2016-05-12 22:03:55 +0200

    Removed UserService

--- usermanager/src/main/java/dk/trustworks/usermanager/UserApplication.java
+++ usermanager/src/main/java/dk/trustworks/usermanager/UserApplication.java
@@ -1,39 +1,16 @@
 package dk.trustworks.usermanager;
 
 import com.google.common.net.MediaType;
-import com.vaadin.server.VaadinServlet;
 import dk.trustworks.framework.BaseApplication;
-import dk.trustworks.framework.persistence.Helper;
 import dk.trustworks.usermanager.handlers.SalaryHandler;
 import dk.trustworks.usermanager.handlers.UserHandler;
-import dk.trustworks.usermanager.web.UserList;
 import io.undertow.Handlers;
 import io.undertow.Undertow;
 import io.undertow.UndertowOptions;
-import io.undertow.server.HttpHandler;
-import io.undertow.server.handlers.cache.DirectBufferCache;
-import io.undertow.server.handlers.resource.*;
-import io.undertow.servlet.Servlets;
-import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
 import io.undertow.util.Headers;
-import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.RetryNTimes;
-import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
-import org.apache.curator.x.discovery.ServiceInstance;
 import org.apache.curator.x.discovery.ServiceProvider;
-import org.apache.curator.x.discovery.UriSpec;
 import org.xnio.Options;
-
-import java.io.File;
-import java.io.InputStream;
-import java.time.Duration;
-import java.util.Properties;
-
-import static io.undertow.servlet.Servlets.defaultContainer;
-import static io.undertow.servlet.Servlets.deployment;
-import static io.undertow.servlet.Servlets.servlet;
 
 /**
  * Created by hans on 16/03/15.
