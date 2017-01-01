274ec6a659642d3ea9b83bd161148ad5c506a303
hans <hans@godfather.dk>
2017-01-01 21:09:36 +0100

    Changed version to 2.2.0

--- servicemanager/src/main/java/dk/trustworks/servicemanager/ServiceApplication.java
+++ servicemanager/src/main/java/dk/trustworks/servicemanager/ServiceApplication.java
@@ -166,7 +166,7 @@ public class ServiceApplication {
         @Override
         public void handleRequest(final HttpServerExchange exchange) throws Exception {
             exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
-            exchange.getResponseSender().send("{version:2.1.0}");
+            exchange.getResponseSender().send("{version:2.2.0}");
         }
     }
 }
