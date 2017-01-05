e2c61c89960cd53f93b4f8e837f11e316be231b3
hans <hans@godfather.dk>
2017-01-05 23:09:29 +0100

    Migrated to trustworks-framework

--- personalassistant/src/main/java/dk/trustworks/personalassistant/jobs/CheckBudgetJob.java
+++ personalassistant/src/main/java/dk/trustworks/personalassistant/jobs/CheckBudgetJob.java
@@ -1,23 +1,8 @@
 package dk.trustworks.personalassistant.jobs;
 
 import allbegray.slack.SlackClientFactory;
-import allbegray.slack.type.*;
 import allbegray.slack.webapi.SlackWebApiClient;
-import allbegray.slack.webapi.method.chats.ChatPostMessageMethod;
 import dk.trustworks.personalassistant.client.RestClient;
-import dk.trustworks.personalassistant.dto.timemanager.*;
-import dk.trustworks.personalassistant.dto.timemanager.User;
-import net.objectlab.kit.datecalc.common.DateCalculator;
-import net.objectlab.kit.datecalc.common.DefaultHolidayCalendar;
-import net.objectlab.kit.datecalc.common.HolidayCalendar;
-import net.objectlab.kit.datecalc.common.HolidayHandlerType;
-import net.objectlab.kit.datecalc.joda.LocalDateKitCalculatorsFactory;
-import org.apache.commons.lang3.StringUtils;
-import org.joda.time.DateTime;
-import org.joda.time.LocalDate;
-import org.jooby.quartz.Scheduled;
-
-import java.util.*;
 
 /**
  * Created by hans on 15/06/16.
