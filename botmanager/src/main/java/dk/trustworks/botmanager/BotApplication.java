1bddec0dafd466f575e12bb4fe69bf7c3be4d189
hans <hans@godfather.dk>
2017-01-21 12:29:45 +0100

    Fixed cronjob

--- botmanager/src/main/java/dk/trustworks/botmanager/BotApplication.java
+++ botmanager/src/main/java/dk/trustworks/botmanager/BotApplication.java
@@ -1,6 +1,7 @@
 package dk.trustworks.botmanager;
 
 import dk.trustworks.botmanager.jobs.CheckBudgetJob;
+import org.quartz.CronTrigger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,23 +36,23 @@ public class BotApplication {
     }
 
     @Bean
-    public JobDetailFactoryBean jobDetailFactoryBean2(){
-        JobDetailFactoryBean factory = new JobDetailFactoryBean();
-        factory.setJobClass(CheckBudgetJob.class);
-        factory.setGroup("mygroup");
-        factory.setName("myjob2");
-        return factory;
-    }
-
-    @Bean
     public CronTriggerFactoryBean cronTriggerFactoryBean(){
         CronTriggerFactoryBean stFactory = new CronTriggerFactoryBean();
         stFactory.setJobDetail(jobDetailFactoryBean().getObject());
         stFactory.setStartDelay(3000);
         stFactory.setName("mytrigger");
         stFactory.setGroup("mygroup");
-        stFactory.setCronExpression("0 0 10 20W * ?");
+        stFactory.setCronExpression("0 0 10 23W * ?");
         return stFactory;
+    }
+
+    @Bean
+    public JobDetailFactoryBean jobDetailFactoryBean2(){
+        JobDetailFactoryBean factory = new JobDetailFactoryBean();
+        factory.setJobClass(CheckBudgetJob.class);
+        factory.setGroup("mygroup2");
+        factory.setName("myjob2");
+        return factory;
     }
 
     @Bean
@@ -59,7 +61,7 @@ public class BotApplication {
         stFactory.setJobDetail(jobDetailFactoryBean2().getObject());
         stFactory.setStartDelay(3000);
         stFactory.setName("mytrigger2");
-        stFactory.setGroup("mygroup");
+        stFactory.setGroup("mygroup2");
         stFactory.setCronExpression("0 0 10 10W * ?");
         return stFactory;
     }
@@ -67,8 +69,9 @@ public class BotApplication {
     @Bean
     public SchedulerFactoryBean schedulerFactoryBean() {
         SchedulerFactoryBean scheduler = new SchedulerFactoryBean();
-        scheduler.setTriggers(cronTriggerFactoryBean().getObject());
-        scheduler.setTriggers(cronTriggerFactoryBean2().getObject());
+        CronTrigger cronTrigger = cronTriggerFactoryBean().getObject();
+        CronTrigger cronTrigger2 = cronTriggerFactoryBean2().getObject();
+        scheduler.setTriggers(cronTrigger, cronTrigger2);
         return scheduler;
     }
 }
